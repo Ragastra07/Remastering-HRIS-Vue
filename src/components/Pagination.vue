b4a1a8a29b4de6b3b7e1c353ee6132f0a572a380
Ragastra07 <haryo.accn.7@gmail.com>
2024-12-09 01:08:09 +0700

    first touch

---
+++ src/components/Pagination.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav>
+        <ul :class="`pagination justify-content-${align} mb-0`">
+            <li :class="[
+                    'page-item',
+                    link.url == null ? 'disabled' : '',
+                    link.active ? 'active' : '',
+                ]"
+                v-for="(link, index) in links" :key="index">
+                <Link
+                    class="page-link"
+                    :href="link.url === null ? '#' : link.url"
+                    v-html="link.label">
+                </Link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+    //import Link
+    import { Link } from '@inertiajs/inertia-vue3';
+
+    export default {
+        props: {
+            links: Array,
+            align: String
+        },
+
+        components: {
+            Link,
+        },
+    }
+</script>
+
+<style>
+    @media only screen and (max-width: 575px) {
+        .pagination{
+            width: fit-content;
+            overflow:visible;
+            overflow-x: visible;
+        }
+    }
+</style>
